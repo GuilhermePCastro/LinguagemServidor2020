f42a7eede8f14ffa416234a17d7ef01674822288
Guilherme Pereira de Castro <63517384+GuilhermePCastro@users.noreply.github.com>
2020-08-20 22:11:11 -0300

    Primeira aula de PHP - Exemplos de operadores na web

---
+++ operadores/index.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "Olá Mundo!";
+
+/*
+    Comentário em bloco
+*/
+
+// comentário em linha
+
+$Saldo = 34.5;
+
+Print("<br><br><br> Seu Saldo é: $Saldo");
+
+echo '<br><br><br> Seu Saldo é: $Saldo';
+
+$Saldo += 10;
+
+Print("<br><br><br> Agora seu Saldo é: $Saldo");
+
